f1972c6a95541a8579fbd53bc9c3ce9117c7840d
Diana <dianakaro211@gmail.com>
2026-06-18 08:59:27 +0200

    Scanner gesplittet und code in tokenizer hinzugefügt

--- src/main/java/de/couven/scanner/Scanner.java
+++ src/main/java/de/couven/scanner/Scanner.java
@@ -1,15 +1,10 @@
 package de.couven.scanner;
 
 import de.couven.token.Position;
-import de.couven.token.Token;
-import de.couven.token.TokenType;
 
-import java.util.ArrayList;
-import java.util.List;
 
 public class Scanner {
-
-    private String input;
+    private final String input;
     private int pos = 0;
     private int line = 1;
     private int col = 1;
@@ -18,7 +13,7 @@ public class Scanner {
         this.input = input;
     }
 
-    private char current() {
+    public char current() {
 
         if (pos >= input.length())
             return '\0';
@@ -26,7 +21,7 @@ public class Scanner {
         return input.charAt(pos);
     }
 
-    private void advance() {
+    public void advance() {
         if (current() == '\n') {
             line++;
             col = 1;
@@ -36,161 +31,15 @@ public class Scanner {
         pos++;
     }
 
-    private Position here() {
+    public Position here() {
         return new Position(line, col);
     }
 
-    private void skipWhitespace() {
+    public void skipWhitespace() {
 
         while (Character.isWhitespace(current())) {
             advance();
         }
     }
 
-    private Token readNumber() {
-
-        Position start = here();
-        StringBuilder sb = new StringBuilder();
-
-        while (Character.isDigit(current())) {
-            sb.append(current());
-            advance();
-        }
-
-        return new Token(TokenType.ZAHL, sb.toString(), start);
-    }
-
-    private Token readIdentifier() {
-
-        Position start = here();
-        StringBuilder sb = new StringBuilder();
-
-        while (Character.isLetterOrDigit(current())
-                || current() == '_') {
-
-            sb.append(current());
-            advance();
-        }
-
-        String word = sb.toString();
-
-        switch (word) {
-
-            case "if":
-                return new Token(TokenType.WENN, word, start);
-
-            case "else":
-                return new Token(TokenType.SONST, word, start);
-
-            case "while":
-                return new Token(TokenType.SOLANGE, word, start);
-
-            case "class":
-                return new Token(TokenType.KLASSE, word, start);
-
-            case "void":
-            case "int":
-                return new Token(TokenType.TYP, word, start);
-
-            default:
-                return new Token(TokenType.NAME, word, start);
-        }
-    }
-
-    public List<Token> scan() {
-
-        List<Token> tokens = new ArrayList<>();
-
-        while (current() != '\0') {
-
-            skipWhitespace();
-
-            if (current() == '\0')
-                break;
-
-            if (Character.isDigit(current())) {
-                tokens.add(readNumber());
-                continue;
-            }
-
-            if (Character.isLetter(current())) {
-                tokens.add(readIdentifier());
-                continue;
-            }
-
-            Position start = here();
-
-            switch (current()) {
-
-                case '=':
-                    advance();
-
-                    if (current() == '=') {
-                        advance();
-                        tokens.add(
-                                new Token(TokenType.VERGLOP, "==", start));
-                    } else {
-                        tokens.add(
-                                new Token(TokenType.ZUWEISUNGSOP, "=", start));
-                    }
-                    break;
-
-                case '+':
-                    tokens.add(new Token(TokenType.STRICHOP, "+", start));
-                    advance();
-                    break;
-
-                case '-':
-                    tokens.add(new Token(TokenType.STRICHOP, "-", start));
-                    advance();
-                    break;
-
-                case '*':
-                    tokens.add(new Token(TokenType.PUNKTOP, "*", start));
-                    advance();
-                    break;
-
-                case '/':
-                    tokens.add(new Token(TokenType.PUNKTOP, "/", start));
-                    advance();
-                    break;
-
-                case '(':
-                    tokens.add(new Token(TokenType.KLAMMERAUF, "(", start));
-                    advance();
-                    break;
-
-                case ')':
-                    tokens.add(new Token(TokenType.KLAMMERZU, ")", start));
-                    advance();
-                    break;
-
-                case '{':
-                    tokens.add(new Token(TokenType.BLOCKAUF, "{", start));
-                    advance();
-                    break;
-
-                case '}':
-                    tokens.add(new Token(TokenType.BLOCKZU, "}", start));
-                    advance();
-                    break;
-
-                case ';':
-                    tokens.add(
-                            new Token(TokenType.SEMIKOLON, ";", start));
-                    advance();
-                    break;
-
-                default:
-                    tokens.add(new Token(
-                            TokenType.FEHLER, String.valueOf(current()), start));
-                    advance();
-                    break;
-            }
-        }
-
-        tokens.add(new Token(TokenType.EOF, "", here()));
-
-        return tokens;
-    }
 }

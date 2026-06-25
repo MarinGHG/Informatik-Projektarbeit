169c222430f54dbae458a2b48f00d1e19a9725c4
Marin Benke <benkemarin@gmail.com>
2026-06-25 10:12:36 +0200

    feat(gui): Token-Lexikon-Panel + HiDPI-Fix fuer Wayland/Linux
    - Einblendbares Token-Dictionary (Tabelle: Nr., Token, Beispiel) per Toggle-Button
    - HiDPI-Properties in Main.java fuer korrekte Skalierung unter Wayland
    - run.sh Startskript mit expliziten JVM-HiDPI-Flags

--- src/main/java/de/couven/Main.java
+++ src/main/java/de/couven/Main.java
@@ -4,6 +4,9 @@ import de.couven.gui.MainWindow;
 
 public class Main {
     public static void main(String[] args) {
+        System.setProperty("sun.java2d.uiScale.enabled", "true");
+        System.setProperty("awt.useSystemAAFontSettings", "on");
+        System.setProperty("swing.aatext", "true");
         new MainWindow();
     }
 }
